0c6e4f6b8641e1a266c947c7e3937765b03d4c5b
supvolume <47323852+supvolume@users.noreply.github.com>
2019-02-11 21:22:13 +0700

    Create nbMonths.r

---
+++ 6kyu/nbMonths.r
@@ -0,0 +1,23 @@
+#solution for Buying a car challenge
+#not finish
+
+nbMonths <- function(priceOld, priceNew, saving, loss) {
+newSaving <- saving
+if(priceOld >= priceNew){
+return(c(0,priceOld-priceNew))
+} else {
+    month <- 1
+    priceOld <- priceOld - (priceOld*loss/100)
+    priceNew <- priceNew - (priceNew*loss/100)
+    while((priceOld + newSaving) < priceNew){
+        month <- month + 1
+        if(month%%2 == 0){
+            loss <- loss + 0.5
+        }
+        priceOld <- priceOld - (priceOld*loss/100)
+        priceNew <- priceNew - (priceNew*loss/100)
+        newSaving <- newSaving + saving
+    }
+return(c(month,floor(priceOld+newSaving-priceNew)))
+}
+}
